11a2de3630a3e0c39e61ae94aa82c887b8379be5
Ismail40376 <dlatiktoka304@gmail.com>
2025-04-29 12:57:43 +0500

    inplement test for get product by id

--- tests/unit/product.test.js
+++ tests/unit/product.test.js
@@ -37,4 +37,31 @@ describe("Product handlers - unit tests", () => {
       expect(res.sendStatus).toHaveBeenCalledWith(500);
     });
   });
+
+  describe("getProductById", () => {
+    test("return found product", async () => {
+      const fake = { _id: "xyz" };
+      Product.findById.mockResolvedValue(fake);
+
+      const req = { params: { id: "xyz" } };
+      const res = { send: jest.fn(), sendSatatus: jest.fn() };
+      await getProductById(req, res);
+      expect(Product.findById).toHaveBeenCalledWith("xyz");
+      expect(res.send).toHaveBeenCalledWith(fake);
+    });
+    test("return 404 if product not found", async () => {
+      Product.findById.mockResolvedValue(null);
+      const req = { params: { id: "nope" } };
+      const res = { send: jest.fn(), sendStatus: jest.fn() };
+      await getProductById(req, res);
+      expect(res.sendStatus).toHaveBeenCalledWith(404);
+    });
+    test("return 500", async () => {
+      Product.findById.mockRejectedValue(new Error("err"));
+      const req = { params: { id: "123" } };
+      const res = { send: jest.fn(), sendStatus: jest.fn() };
+      await getProductById(req, res);
+      expect(res.sendStatus).toHaveBeenCalledWith(500);
+    });
+  });
 });
